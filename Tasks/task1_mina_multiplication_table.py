319092fc1220046b9a223e043344d1aca1041a0c
ABC <abc@gmail.com>
2026-08-16 01:11:01 +0300

    session_5_assignment_part_II

--- Tasks/task1_mina_multiplication_table.py
+++ Tasks/task1_mina_multiplication_table.py
@@ -1,12 +1,24 @@
-def calculate_multiplication_table(num:int)->None:
-    ''' 
-    This Function calculate the multiplication table for every number and prints it
-    Args:
-        num(int): The number from User
-    
-    '''
-    for i in range (1,11):
-        print(f"{num} * {i} = {num*i}")
+class MultiplicationTable:
+    ''' This class get the multiplication table of a number'''
+    def __init__(self,num):
+        self._num = num
+    def calculate_multiplication_table(self)->None:
+        ''' 
+        This Function calculate the multiplication table for every number and prints it
+        Args:
+            num(int): The number from User
+        
+        '''
+
+        num = self._num
+
+        for i in range (1,11):
+            print(f"{num} * {i} = {num*i}")
 
 if __name__ == '__main__':
-    calculate_multiplication_table(6)
+    mt1 = MultiplicationTable(7)
+    mt2 = MultiplicationTable(11)
+
+    mt1.calculate_multiplication_table()
+    print("========================")
+    mt2.calculate_multiplication_table()
